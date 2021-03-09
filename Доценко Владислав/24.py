d90ba007e1a645d76039cce03de61425d0adce47
Vladislav-Dots <57825121+Vladislav-Dots@users.noreply.github.com>
2021-03-09 22:56:27 +0500

    Update 24.py
    Исправил

--- Доценко Владислав/24.py
+++ Доценко Владислав/24.py
@@ -5,10 +5,10 @@ while(True):
     skidka_percent   = int(input("Скидка в % = ")) # от 0 до 100
     if(skidka_percent < 100 and skidka_percent >= 0):
         break
-summa            = float(input("Сумма в $ = "))
-
+summa_start      = float(input("Сумма в $ = "))
+summa_end = summa_start
 for day in range(1,days):
-    summa += 3
-    summa *= (1 - skidka_percent / 100)
+    summa_end += 3
+summa_end *= (1 - skidka_percent / 100)
 
-print("Итоговая сумма = %f ", summa)
+print("Прибыль = %.2f", %(summa_end - summa_start))
